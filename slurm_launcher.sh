80e3089614a70d5307abec78d8301fff9de40dd2
lionelblonde <lionel.blonde@gmail.com>
2023-08-15 09:15:01 +0200

    Modify the default simclr pre-trained weights to load

--- slurm_launcher.sh
+++ slurm_launcher.sh
@@ -5,7 +5,8 @@ export DATASET_DIR=/share/users/${USER:0:1}/${USER}
 # export MODEL_DIR=/home/users/b/blondeli/Code/geo-pytorch/data/checkpoints/weepoo_zoofer_shoophu.gitSHA_81c7d86.simclr_1.seed00
 # export MODEL_DIR=/home/users/b/blondeli/Code/geo-pytorch/data/checkpoints/booshe_fapoo_hoozu.gitSHA_cf6299e.simclr_1.seed00
 # export MODEL_DIR=/home/users/b/blondeli/Code/geo-pytorch/data/checkpoints/serchoo_huze_woochoo.gitSHA_50f8ca5.simclr_1.seed00
-export MODEL_DIR=/home/users/b/blondeli/Code/geo-pytorch/data/checkpoints/pheetu_zuhoo_chooher.gitSHA_9653a1e.simclr_1.seed00
+# export MODEL_DIR=/home/users/b/blondeli/Code/geo-pytorch/data/checkpoints/pheetu_zuhoo_chooher.gitSHA_9653a1e.simclr_1.seed00
+export MODEL_DIR=/home/users/b/blondeli/Code/geo-pytorch/data/checkpoints/sheepa_sashu_soosu.gitSHA_a9a0107.simclr_1.seed00
 # the latter var is not always used but need be here for some configs
 
 python spawner.py \
