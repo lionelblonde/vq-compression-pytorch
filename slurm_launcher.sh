afa3c6bc7fd1dab58ab1fec6e7afdc1bd0f1b223
lionelblonde <lionel.blonde@me.com>
2023-12-08 23:23:22 +0100

    Correct name of path leading to run config

--- slurm_launcher.sh
+++ slurm_launcher.sh
@@ -6,7 +6,7 @@ export MODEL_DIR=/home/users/b/blondeli/Code/geo-pytorch/data/checkpoints/sheepa
 # the latter var is not always used but need be here for some configs
 
 python spawner.py \
-    --config configs/vqvae/vqvae.yaml \
+    --config configs/compression.yaml \
     --conda_env geocuda \
     --deployment slurm \
     --num_seeds 1 \
