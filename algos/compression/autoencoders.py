1ba5c8d3022351ee0a96d28b19138c283115054b
lionelblonde <lionel.blonde@me.com>
2023-12-12 17:55:46 +0100

    Clean up autoencoder model

--- algos/compression/autoencoders.py
+++ algos/compression/autoencoders.py
@@ -6,15 +6,14 @@ from algos.compression.models import EncoderModel, DecoderModel
 from algos.compression.quantizers import VectorQuantizer
 from algos.compression.histograms import Histogram
 
-from vector_quantize_pytorch import ResidualVQ  # TODO: use this to integrate ResidualVQ
+from vector_quantize_pytorch import ResidualVQ  # from lucidrains
 
 
 class VectorQuantizationAutoEncoder(nn.Module):
 
-    def __init__(self, hps, device):
+    def __init__(self, hps):
         super().__init__()
         self.hps = hps
-        self.device = device
 
         # XXX: this is temporary
         self.hps.residual_vq = True
@@ -25,11 +24,11 @@ class VectorQuantizationAutoEncoder(nn.Module):
         self.hps.threshold_ema_dead_code = 2
         self.hps.learnable_codebook = False
 
-        self.encoder = EncoderModel(self.hps).to(self.device)
-        self.decoder = DecoderModel(self.hps).to(self.device)
+        self.encoder = EncoderModel(self.hps)
+        self.decoder = DecoderModel(self.hps)
 
         if self.hps.residual_vq:
-            # lucidrains: this follows Algo 1 in the SoundStream paper
+            # this follows algo 1 in the SoundStream paper
             self.vector_quantizer = ResidualVQ(
                 # args specific to the residual extension
                 num_quantizers=self.hps.num_quantizers,  # no defaults
@@ -43,10 +42,11 @@ class VectorQuantizationAutoEncoder(nn.Module):
                 ema_update=True,  # default: True
                 learnable_codebook=self.hps.learnable_codebook,  # default: False
             )
-        else:  # really aligned with the base vector quantizer, no extras
-            self.vector_quantizer = VectorQuantizer(self.hps).to(self.device)
+        else:
+            # this is the implementation of a non-variational VQ-VAE
+            self.vector_quantizer = VectorQuantizer(self.hps)
 
-        self.histogram = Histogram(self.hps).to(self.device)
+        self.histogram = Histogram(self.hps)
 
     def forward(self, x):
         z = self.encoder(x)
@@ -79,14 +79,14 @@ class VectorQuantizationAutoEncoder(nn.Module):
             losses['vq_loss'] = vq_loss
             loss = recon_error + vq_loss
 
-        if self.hps.beta > 0:
-            ce = self.histogram.ce(encoding_indices)
-            loss += self.hps.beta * ce
-            losses['ce'] = ce
-        if self.hps.alpha > 0:
-            soft_ce = self.histogram.soft_ce(normalized_proximities)
-            loss += self.hps.alpha * soft_ce
-            losses['soft_ce'] = soft_ce
+            if self.hps.beta > 0:
+                ce = self.histogram.ce(encoding_indices)
+                loss += self.hps.beta * ce
+                losses['ce'] = ce
+            if self.hps.alpha > 0:
+                soft_ce = self.histogram.soft_ce(normalized_proximities)
+                loss += self.hps.alpha * soft_ce
+                losses['soft_ce'] = soft_ce
 
         losses['loss'] = loss
 
